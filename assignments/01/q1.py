1dcbda82d00ed3b2c643c9477ddd7770bf2c953f
shitingxun <TingxunShi@gmail.com>
2018-01-25 21:21:13 +0800

    Finished q1

--- assignments/01/q1.py
+++ assignments/01/q1.py
@@ -7,9 +7,10 @@ cs20.stanford.edu
 Created by Chip Huyen (chiphuyen@cs.stanford.edu)
 """
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 import tensorflow as tf
+import numpy as np
 
 sess = tf.InteractiveSession()
 ###############################################################################
@@ -30,7 +31,12 @@ print(sess.run(out))
 # Hint: Look up tf.case().
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform([], -1, 1)
+y = tf.random_uniform([], -1, 1)
+out = tf.case({tf.less(x, y): lambda: x + y,
+               tf.greater(x, y): lambda: x - y},
+              default=lambda: 0.)
+print(sess.run(out))
 
 ###############################################################################
 # 1c: Create the tensor x of the value [[0, -2, -1], [0, 1, 2]] 
@@ -39,7 +45,11 @@ print(sess.run(out))
 # Hint: Look up tf.equal().
 ###############################################################################
 
-# YOUR CODE
+x = tf.get_variable("x1", initializer=[[0, -2, -1], [0, 1, 2]], dtype=tf.int32)
+x.initializer.run()
+y = tf.zeros(x.get_shape(), name="y1", dtype=tf.int32)
+out = tf.equal(x, y)
+print(sess.run(out))
 
 ###############################################################################
 # 1d: Create the tensor x of value 
@@ -54,7 +64,18 @@ print(sess.run(out))
 # Hint: Use tf.gather().
 ###############################################################################
 
-# YOUR CODE
+array = np.array([29.05088806,  27.61298943,  31.19073486,  29.35532951,
+                  30.97266006,  26.67541885,  38.08450317,  20.74983215,
+                  34.94445419,  34.45999146,  29.06485367,  36.01657104,
+                  27.88236427,  20.56035233,  30.20379066,  29.51215172,
+                  33.71149445,  28.59134293,  36.05556488,  28.66994858])
+x = tf.get_variable("x2", initializer=array)
+x.initializer.run()
+y = tf.get_variable("y2", initializer=np.array([30.] * len(array)))
+y.initializer.run()
+indices = tf.where(tf.greater(x, 30))
+out = tf.gather(x, indices)
+print(sess.run(out))
 
 ###############################################################################
 # 1e: Create a diagnoal 2-d tensor of size 6 x 6 with the diagonal values of 1,
@@ -62,7 +83,8 @@ print(sess.run(out))
 # Hint: Use tf.range() and tf.diag().
 ###############################################################################
 
-# YOUR CODE
+x = tf.diag(tf.range(1, 7))
+print(sess.run(x))
 
 ###############################################################################
 # 1f: Create a random 2-d tensor of size 10 x 10 from any distribution.
@@ -70,7 +92,8 @@ print(sess.run(out))
 # Hint: Look at tf.matrix_determinant().
 ###############################################################################
 
-# YOUR CODE
+x = tf.random_uniform((10, 10), -1, 1)
+print(sess.run(tf.matrix_determinant(x)))
 
 ###############################################################################
 # 1g: Create tensor x with value [5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9].
@@ -78,7 +101,10 @@ print(sess.run(out))
 # Hint: use tf.unique(). Keep in mind that tf.unique() returns a tuple.
 ###############################################################################
 
-# YOUR CODE
+x = tf.get_variable("x3", initializer=[5, 2, 3, 5, 10, 6, 2, 3, 4, 2, 1, 1, 0, 9])
+x.initializer.run()
+t, _ = tf.unique(x)
+print(sess.run(t))
 
 ###############################################################################
 # 1h: Create two tensors x and y of shape 300 from any normal distribution,
@@ -90,4 +116,13 @@ print(sess.run(out))
 # Hint: see the Huber loss function in the lecture slides 3.
 ###############################################################################
 
-# YOUR CODE
+x = tf.get_variable("x4", shape=[300], initializer=tf.random_normal_initializer)
+y = tf.get_variable("y4", shape=[300], initializer=tf.random_normal_initializer)
+x.initializer.run()
+y.initializer.run()
+t = tf.cond(
+    tf.less(tf.reduce_mean(x - y), 0),
+    lambda: tf.reduce_mean(tf.squared_difference(x, y)),
+    lambda: tf.reduce_sum(tf.abs(x - y))
+)
+print(sess.run(t))
